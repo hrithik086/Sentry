11b4adb294c5dc3d6d8b3a979df78ddceac85330
K Hrithik Rao <hrithik@Ks-Mac-mini.local>
2026-01-29 09:09:30 +0530

    Partial Commit: Filemanager Implementation 01

--- Sentry/src/app/Component/login/login.ts
+++ Sentry/src/app/Component/login/login.ts
@@ -1,18 +1,45 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { FileManager } from '../../Services/FileManager/file-manager';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [MatFormField, MatLabel, MatIcon],
+  imports: [MatFormField, MatLabel, MatIcon, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
   private fileManager: FileManager = inject(FileManager);
 
+  public readonly loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: [''],
+      password: [''],
+      uploadedFile: [null, Validators.required]
+    });
+  }
+
   websiteTitle: string = 'SENTRY';
   tagLine: string = 'Securely manage your credentials';
   hide: boolean = true;
+
+  public onFileChange(event: any): void {
+    const inputElement: HTMLInputElement = event.target as HTMLInputElement;
+    if (inputElement.files && inputElement.files.length > 0) {
+      const file = inputElement.files[0];
+      this.loginForm.patchValue({
+        uploadedFile: file
+      });
+    }
+  }
+
+  public onSubmit(): void {
+    if(this.loginForm.valid) {
+      this.fileManager.readJsonFile(this.loginForm.value.uploadedFile);
+    }
+  }
 }
--- Sentry/src/app/Services/FileManager/file-manager.ts
+++ Sentry/src/app/Services/FileManager/file-manager.ts
@@ -4,5 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class FileManager {
-  
+  public readJsonFile(file: File): void {
+    let reader = new FileReader();
+    reader.onload = (event: any) => {
+      try {
+        const json = JSON.parse(event.target.result);
+        console.log('JSON content:', json);
+      } catch (e) {
+        console.error('Error parsing JSON:', e);
+      }
+    };
+    reader.readAsText(file);
+  }
 }
