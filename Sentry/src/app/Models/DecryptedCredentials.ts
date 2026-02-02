5abfd84c7faf7b4489ba67a64827fb4d97ccee4e
K Hrithik Rao <hrithik086@gmail.com>
2026-02-02 09:50:40 +0530

    Partial Commit: Filemanager Implementation 04

--- Sentry/src/app/Models/DecryptedCredentials.ts
+++ Sentry/src/app/Models/DecryptedCredentials.ts
@@ -2,8 +2,8 @@ import { Credentials } from "./Credentials";
 import { DecryptedOtherCredentialDetails } from "./DecryptedOtherCredentialDetails";
 
 export class DecryptedCredentials extends Credentials {
-    public dencryptedPassword: string;
+    public decryptedPassword: string;
     public decryptedPin: string;
-    public dencryptedSecurityKeys: string;
+    public decryptedSecurityKeys: string;
     public decryptedOtherDetails: DecryptedOtherCredentialDetails[];
 }
--- Sentry/src/app/Services/FileManager/file-manager.ts
+++ Sentry/src/app/Services/FileManager/file-manager.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal, Signal, WritableSignal } from '@angular/core';
 import { LoginDetailsAndCredentials } from '../../Models/DTO/LoginDetaiilsAndCredentials';
 import { DecryptedCredentials } from '../../Models/DecryptedCredentials';
 import { cloneDeep } from 'lodash';
@@ -11,7 +11,7 @@ import { Credentials } from '../../Models/Credentials';
 export class FileManager {
   private _allCredentialDetails: LoginDetailsAndCredentials;
   private _decryptedCredentials: DecryptedCredentials[];
-  private _encryptedCredentials: Credentials[];
+  private _encryptedCredentials: WritableSignal<Credentials[]>;
 
   public readJsonFile(file: File): void {
     let reader = new FileReader();
@@ -27,9 +27,10 @@ export class FileManager {
     reader.readAsText(file);
   }
 
-  public get allCredentialDetails(): Credentials[] {
-    this._encryptedCredentials = this._encryptedCredentials ?? cloneDeep(this._allCredentialDetails.credentials);
-    return this._encryptedCredentials;
+  public get allCredentialDetails(): Signal<Credentials[]> {
+    if(this._encryptedCredentials == null)
+      this._encryptedCredentials = signal<Credentials[]>(this._allCredentialDetails.credentials);
+    return this._encryptedCredentials.asReadonly();
   }
   
   public decryptedCredentials(domainName: string, userId: string) : string{
@@ -42,14 +43,49 @@ export class FileManager {
         return this.decryptString(credentials[0].password);
       }
     }
+    else{
+      const encryptedCredentialIndex = this._allCredentialDetails.credentials.findIndex((cred) => cred.domainName === domainName && cred.userId === cred.userId);
+      if(encryptedCredentialIndex >= 0){
+        const decryptedCredential = this.decryptString(this._allCredentialDetails.credentials[encryptedCredentialIndex].password);
+        // new DecryptedCredentials{
+          
+        // }
+        //i need create an object of DecryptedCredential to put this object inside the array of decrypted credential which will act as cache
+      }
+    }
     //throw error
     return ""
+  }
+
+  private refreshEncryptedCredentialsSignal() : void{
+    this._encryptedCredentials.set(this._allCredentialDetails.credentials);
   }
 
   public addEncryptedCredential(newCredential: Credentials){
     const checkExistingCredentials = this._allCredentialDetails.credentials.filter((credential) => credential.domainName === newCredential.domainName && credential.userId === newCredential.userId);
     if(checkExistingCredentials.length === 0){
-      
+      this._allCredentialDetails.credentials.push(newCredential);
+      this.refreshEncryptedCredentialsSignal();
+    }
+  }
+
+  public removeEncryptedCredentials(domainName: string, userId: string){
+    const filteredCredentials = this._allCredentialDetails.credentials.filter((cred) => cred.domainName === domainName && cred.userId === userId);
+    if(filteredCredentials.length === 1){
+      let index = this._allCredentialDetails.credentials.findIndex((cred) => cred.domainName==domainName && cred.userId === userId);
+      this._allCredentialDetails.credentials.splice(index, 1);
+
+      index = this._decryptedCredentials.findIndex((cred) => cred.domainName==domainName && cred.userId === userId);
+      if(index >= 0)
+        this._decryptedCredentials.splice(index,1);
+
+      this.refreshEncryptedCredentialsSignal();
+    }
+    else if(filteredCredentials.length > 1){
+      //log error that there are duplicates and we need to remove all and then we need to add one
+    }
+    else{
+      //log error that there is no object which is matching the domain name and userid.
     }
   }
 
