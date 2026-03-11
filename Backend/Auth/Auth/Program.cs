428c3259a4c573cfe78fa1e063fc7f3700557d22
K Hrithik Rao <hrithik086@gmail.com>
2026-03-11 08:52:02 +0530

    changed the sln format to slnx format

--- Backend/Auth/Auth/Program.cs
+++ Backend/Auth/Auth/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -11,8 +10,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
--- Backend/Auth/Auth/obj/Debug/net10.0/Auth.AssemblyInfo.cs
+++ Backend/Auth/Auth/obj/Debug/net10.0/Auth.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae9e46611be6bbccb84db18358a079ebf3adb5b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab12239a439ce4a24d41932c987f41cc070d8363")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
